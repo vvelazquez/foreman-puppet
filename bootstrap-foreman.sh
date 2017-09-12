80278fce5eff6a1051d049be2413d5ae16cd7355
Victor Velazquez <victor@velazquez.ch>
2017-09-12 15:30:41 +0200

    Update bootstrap-foreman.sh

--- bootstrap-foreman.sh
+++ bootstrap-foreman.sh
@@ -7,31 +7,6 @@ echo "@@bootstrap-foreman ... Update system first: yum update -y "
 sudo yum update -y
 
 
-echo "@@bootstrap-foreman ... puppet checks "
-if puppet agent --version | grep "4." | grep -v grep 2> /dev/null
-then
-    echo "@@bootstrap-foreman ... Puppet Agent $(puppet agent --version) is already installed. A la verga de aqui..."
-else
-    echo "@@bootstrap-foreman ... Puppet Agent $(puppet agent --version) installed. Replacing..."
-
-	echo "@@bootstrap-foreman ...  sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm"
-    sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-	echo "@@bootstrap-foreman ...  yum -y install puppetserver puppetdb"
-	sudo yum -y install puppetserver puppetdb
-	
-	echo "@@bootstrap-foreman ...  sudo yum -y erase puppet-agent "
-    sudo yum -y erase puppet-agent && \
-	
-	echo "@@bootstrap-foreman ...  sudo rm -f /etc/yum.repos.d/puppetlabs-pc1.repo"
-    sudo rm -f /etc/yum.repos.d/puppetlabs-pc1.repo && \
-	
-	echo "@@bootstrap-foreman ...  sudo yum clean all"
-    sudo yum clean all
-fi
-
-
-
-
 
 if ps aux | grep "/usr/share/foreman" | grep -v grep 2> /dev/null
 then
@@ -56,6 +31,8 @@ else
     # https://www.digitalocean.com/community/tutorials/additional-recommended-steps-for-new-centos-7-servers
 	
 	echo "@@bootstrap-foreman ... Set-up firewall - add-port... starting"
+	sudo systemctl enable firewalld
+	sudo systemctl start firewalld
     sudo firewall-cmd --permanent --add-service=http
     sudo firewall-cmd --permanent --add-service=https
     sudo firewall-cmd --permanent --add-port=69/tcp
@@ -77,18 +54,18 @@ else
     # automatically creating the host in Foreman's database
 	
 	echo "@@bootstrap-foreman ... ... run the Puppet agent on the Foreman host"
-    sudo puppet agent --test --waitforcert=60
+    /opt/puppetlabs/bin/puppet agent --test --waitforcert=60
 
     # Optional, install some optional puppet modules on Foreman server to get started...
-		# echo "... install some optional puppet modules"
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-ntp
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-git
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-vcsrepo
-		# sudo puppet module install -i /etc/puppet/environments/production/modules garethr-docker
-		# sudo puppet module install -i /etc/puppet/environments/production/modules jfryman-nginx
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-haproxy
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-apache
-		# sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-java
+	echo "... install some optional puppet modules"
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-ntp
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-git
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-vcsrepo
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules garethr-docker
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules jfryman-nginx
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-haproxy
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-apache
+	sudo /opt/puppetlabs/bin/puppet module install -i /etc/puppet/environments/production/modules puppetlabs-java
 fi
 
 
