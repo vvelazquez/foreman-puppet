d7aeaba43b2772341f4a068cbc7566bbcc77d1e9
Victor Velazquez <victor@velazquez.ch>
2017-09-12 16:57:31 +0200

    Update bootstrap-node-rhel.sh

--- bootstrap-node-rhel.sh
+++ bootstrap-node-rhel.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
+echo "@@bootstrap-node-rhel. ... Update system first"
 # Update system first
 sudo yum update -y
 
 
-# Add agent section to /etc/puppet/puppet.conf
+# Add agent section to /etc/puppetlabs/puppet/puppet.conf
 # Easier to set run interval to 120s for testing (reset to 30m for normal use)
 # https://docs.puppetlabs.com/puppet/3.8/reference/config_about_settings.html
+echo "@@bootstrap-node-rhel. ... Add agent section to /etc/puppetlabs/puppet/puppet.conf"
 echo "" | sudo tee --append /etc/puppetlabs/puppet/puppet.conf 2> /dev/null && \
 echo "    server = theforeman.example.com" | sudo tee --append /etc/puppetlabs/puppet/puppet.conf 2> /dev/null && \
 echo "    runinterval = 120s" | sudo tee --append /etc/puppetlabs/puppet/puppet.conf 2> /dev/null
 
+echo "@@bootstrap-node-rhel. ... sudo service puppet stop"
 sudo service puppet stop
 #sudo service puppet start
-sudo puppet resource service puppet ensure=running enable=true
-sudo puppet agent --enable
+
+echo "@@bootstrap-node-rhel. ... sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true"
+sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+
+echo "@@bootstrap-node-rhel. ... sudo /opt/puppetlabs/bin/puppet agent --enable"
+sudo /opt/puppetlabs/bin/puppet agent --enable
 
 # Unless you have Foreman autosign certs, each agent will hang on this step until you manually
 # sign each cert in the Foreman UI (Infrastrucutre -> Smart Proxies -> Certificates -> Sign)
